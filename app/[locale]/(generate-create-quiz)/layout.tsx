d61e8faf9fb56c0a89bdc244e3be6128672c13c9
k0bek <kubakiebala@gmail.com>
2024-09-16 12:56:52 +0200

    feat: add layouts

--- app/[locale]/(generate-create-quiz)/layout.tsx
+++ app/[locale]/(generate-create-quiz)/layout.tsx
@@ -2,6 +2,9 @@ import Header from "@/components/shared/Header";
 import React, { ReactNode } from "react";
 import Stepper from "./(generate-quiz)/components/StatusIndicator/Stepper";
 import StepperProvider from "@/app/context/StepContext";
+import { Metadata } from "next";
+import { routes } from "@/routes";
+
 const layout = ({ children }: { children: ReactNode }) => {
   return (
     <StepperProvider>

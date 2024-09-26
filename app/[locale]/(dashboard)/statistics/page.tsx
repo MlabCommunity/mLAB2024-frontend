07a1ed495699599dd7e713464b8125f429fa1426
Daniel Niedzielski <daniel.niedziels@wp.pl>
2024-09-26 12:57:09 +0200

    Added Statistics link into Header

--- app/[locale]/(dashboard)/statistics/page.tsx
+++ app/[locale]/(dashboard)/statistics/page.tsx
@@ -1,15 +1,33 @@
 "use client";
 import React from "react";
 import { useTranslations } from "next-intl";
+import { motion } from "framer-motion";
+import {
+  Table,
+  TableBody,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  TableCell,
+  Skeleton,
+} from "@nextui-org/react";
+import NavbarContentContainer from "@/components/NavbarContentContainer";
+import DetailsModal from "../../(quiz-details)/modals/DetailsModal";
+import {
+  formatParticipationDate,
+  formatParticipationTime,
+  formatQuizResult,
+} from "@/utils/helpers";
+import StatusChip from "../../(quiz-details)/components/statistics/StatusChip/StatusChip";
+import DetailsButton from "../../(quiz-details)/components/statistics/buttons/DetailsButton";
+import Chart from "../../(quiz-details)/components/chart/Chart";
+import { QuizHistoryType } from "@/types";
 import { useStats } from "../../(quiz-details)/quiz-details/hooks/useStats";
 import Stats from "@/components/shared/Stats";
 
 const StatisticsPage = () => {
   const t = useTranslations("Dashboard");
   const { data: stats, isLoading, isFetching } = useStats();
-  // if (isFetching || isLoading) {
-  //   return <Skeleton className="w-full p-4 h-screen"></Skeleton>;
-  // }
   return (
     <Stats
       quizStats={stats || []}

fe58c7619859c40b151d5d5a70803f2ec7915943
Daniel Niedzielski <daniel.niedziels@wp.pl>
2024-09-26 12:48:59 +0200

    Statistics skeleton changed

--- app/[locale]/(dashboard)/statistics/page.tsx
+++ app/[locale]/(dashboard)/statistics/page.tsx
@@ -28,15 +28,15 @@ import Stats from "@/components/shared/Stats";
 const StatisticsPage = () => {
   const t = useTranslations("Dashboard");
   const { data: stats, isLoading, isFetching } = useStats();
-  if (!stats || stats.length == 0) {
-    return (
-      <div className="flex w-full  font-semibold text-foreground-600 text-2xl items-center justify-center">
-        {t("noDataAvailable")}
-      </div>
-    );
-  }
+  // if (isFetching || isLoading) {
+  //   return <Skeleton className="w-full p-4 h-screen"></Skeleton>;
+  // }
   return (
-    <Stats quizStats={stats} isLoading={isLoading} isFetching={isFetching} />
+    <Stats
+      quizStats={stats || []}
+      isLoading={isLoading}
+      isFetching={isFetching}
+    />
   );
 };
 
